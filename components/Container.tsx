a16f20e89a615b20b04799eefed64ff46103eb84
Mikkel <mikkelkulk@gmail.com>
2022-02-15 18:55:30 +0100

    Page title

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import React from 'react'
 import Header from './Header'
 
@@ -6,6 +7,11 @@ type Props = { children: JSX.Element }
 function Container({ children }: Props) {
   return (
     <div id='root'>
+      <Head>
+        <title>Another CheckList App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Another CheckList App" key="title" />
+      </Head>
       <Header />
       <main className='max-w-7xl mx-auto text-white'>
         {children}
